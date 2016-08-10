8ebc9705c2c9a24967d0f5db0c6dddca5f7acdb7
Cradanimal <cradwimb@gmail.com>
2016-08-10 09:25:59 -0700

    res send on /

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 8080;
 
 
 require('./config/middleware.js')(app, express);
+app.get('/', function(req, res){
+  res.send('I see you standing on the wall... clap, clap you deserve it all!')
+})
 app.use('/api', Router);
 
 app.listen(port, console.log('listening to localhost:' +port));
