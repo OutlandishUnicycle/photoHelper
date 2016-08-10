8b3ce392dc642b42040607b8deacc87e082454f6
Cradanimal <cradwimb@gmail.com>
2016-08-09 22:01:12 -0700

    (feat) successfully posts to s3 from discollect

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,9 +1,11 @@
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var cors = require('cors')
 
 module.exports = function(app, express) {
-  console.log('in the middle');
-
-  app.use(bodyParser.json());
+  app.use(cors());
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   app.use(morgan('dev'));
+  console.log('in the middle');
 };
